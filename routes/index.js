59fa8b90119ccc0673710e3202355b23492da348
Phan Thong Thanh <phanthongthanh0606@gmail.com>
2021-04-03 00:34:46 +0700

    Add default route

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ router.use("/api", require("./api"));
 
 router.use("/auth", require("./auth.js"));
 
+router.get("/", (req, res) => {
+  res.send("PT Chat application API");
+});
+
 module.exports = router;
